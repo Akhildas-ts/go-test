fe52456ec3e340b275f13348a25436a8e91d18a8
Akhil das <akhildas@Akhils-MacBook-Air.local>
2024-10-22 10:43:55 +0530

    docker

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 	uh := handlers.NewUserHandler(uc)
 	router := gin.Default()
 
-	fmt.Println("hys")
+	fmt.Println("hyss")
 	routes.UserRoutes(router.Group("/"), db, uh)
 
 	router.Run()
